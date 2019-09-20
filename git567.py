53c5b229bef92b14e26c993158b8bb71eadaf36d
Bella Manoim <bella458@yahoo.com>
2019-09-19 23:05:46 -0400

    update git567

--- git567.py
+++ git567.py
@@ -1,5 +1,34 @@
-"""Bella Manoim Test
+"""Bella Manoim 
+   
+
 """
 
-print ("Hello, World!")
-exit()
+import requests
+import json
+
+
+def getRepositories(username):
+    repositoriesResponse = requests.get("https://api.github.com/users/" + username + "/repos")   
+    repositories = json.loads(repositoriesResponse.text)
+    repositorylist = []
+    
+    for repository in repositories:
+        repositorylist.append(repository.get("name"))
+    return repositorylist
+
+def getCommits(username, repositoryName):
+    commitsResponse = requests.get("https://api.github.com/repos/" + username + "/" + repositoryName + "/commits")   
+    commitsText = json.loads(commitsResponse.text)
+    commits = len(commitsText)
+    return (commits)
+
+def main():
+    commitResult = getRepositories("bella458")
+
+    for repository in commitResult:
+        commits = getCommits("bella458",repository)
+        print("Repository: " + repository + " Commits: " + str((commits)))
+
+
+if __name__ == "__main__":
+    main()
