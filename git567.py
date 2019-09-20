190238a44c91e7ee1663e3585c0cc2dbd754349f
Bella Manoim <bella458@yahoo.com>
2019-09-19 23:16:15 -0400

    updated heading

--- git567.py
+++ git567.py
@@ -1,6 +1,12 @@
 """Bella Manoim 
-   
 
+   Git API assignment
+   
+   Requirements:
+   Write a function that will take as input a GitHub user ID. 
+   The output from the function will be a list of the names of the 
+   repositories that the user has, along with the number of commits 
+   that are in each of the listed repositories.
 """
 
 import requests
