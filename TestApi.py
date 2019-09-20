c80fb45d1e99a9940682d46bd21f3cf65686b4f0
Bella Manoim <bella458@yahoo.com>
2019-09-19 23:06:21 -0400

    updated unit tests

--- TestApi.py
+++ TestApi.py
@@ -1,5 +1,24 @@
-"""Bella Manoim Test
+""" Bella Manoim Testing git567
 
 """
+import unittest
 
-print ("Hello, World!")
+from git567 import getCommits, getRepositories
+
+class Test(unittest.TestCase):
+
+    def test_getCommits1(self):
+        assert getCommits('bella458', 'SSW-555') == 4
+
+    def test_getCommits2(self):
+        assert getCommits('bella458', 'SSW567') == 3
+        
+    def test_getRepositories1(self):
+        self.assertIn("SSW-555", getRepositories("bella458"))
+    
+    def test_getRepositories2(self):
+        self.assertIn("SSW567", getRepositories("bella458"))
+
+if __name__ == '__main__':
+    print('Running Unit Tests')
+    unittest.main()
