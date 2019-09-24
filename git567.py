b988efe92eb742452f92f7fb282bc9eed154e573
Bella Manoim <bella458@yahoo.com>
2019-09-23 20:11:14 -0400

    update

--- git567.py
+++ git567.py
@@ -1,5 +1,5 @@
 """Bella Manoim 
-
+   
    Git API assignment
    
    Requirements:
@@ -7,21 +7,27 @@
    The output from the function will be a list of the names of the 
    repositories that the user has, along with the number of commits 
    that are in each of the listed repositories.
+   
+   Commented out some lines I used for testing purposes
 """
 
 import requests
 import json
 
-
 def getRepositories(username):
     repositoriesResponse = requests.get("https://api.github.com/users/" + username + "/repos")   
     repositories = json.loads(repositoriesResponse.text)
     repositorylist = []
+    #print(repositorylist)
     
     for repository in repositories:
-        repositorylist.append(repository.get("name"))
+        #print(repository)
+        #print(repository.get('name'))
+        repo = repository.get('name')
+        repositorylist.append(repo)
     return repositorylist
-
+    
+    
 def getCommits(username, repositoryName):
     commitsResponse = requests.get("https://api.github.com/repos/" + username + "/" + repositoryName + "/commits")   
     commitsText = json.loads(commitsResponse.text)
@@ -29,12 +35,9 @@ def getCommits(username, repositoryName):
     return (commits)
 
 def main():
-    commitResult = getRepositories("bella458")
-
-    for repository in commitResult:
+    for repository in getRepositories("bella458"):
         commits = getCommits("bella458",repository)
         print("Repository: " + repository + " Commits: " + str((commits)))
-
-
+        
 if __name__ == "__main__":
     main()
