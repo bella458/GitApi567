2d75e5f0cc734f5455b9cb7414b082e956e3ecdb
Bella Manoim <bella458@yahoo.com>
2019-09-28 20:53:19 -0400

    updates

--- TestApi.py
+++ TestApi.py
@@ -17,7 +17,7 @@ class TestApi(unittest.TestCase):
 
     #'requests.get' 
     @mock.patch('GitApi567.getCommits')
-    def test_getCommits(self, mockedReq):
+    def test_getCommits1(self, mockedReq):
         #use side effect 
         mockedReq.side_effect = [3]
         mockedReq.return_value = MagicMock(3)
@@ -26,12 +26,31 @@ class TestApi(unittest.TestCase):
         assert mockedReq.repo == 'SSW567'
         self.assertEqual(GitApi567.getCommits("Bella458", "SSW567"), 3)
 
+    #'requests.get' 
+    @mock.patch('GitApi567.getCommits')
+    def test_getCommits2(self, mockedReq):
+        #use side effect 
+        mockedReq.side_effect = [5]
+        mockedReq.return_value = MagicMock(5)
+        # assign value to attribute in the mock
+        mockedReq.configure_mock(repo='Helloworld')
+        assert mockedReq.repo == 'Helloworld'
+        self.assertEqual(GitApi567.getCommits("Bella458", "Helloworld"), 5)
+
     #'requests.get'
     @mock.patch('GitApi567.getRepositories')   
-    def test_getRepositories(self, mockedReq):
-        mockedReq.return_value = ["SSW567"]
+    def test_getRepositories1(self, mockedReq):
+        mockedReq.return_value = mock.Mock(["SSW567"])
         mockedReq.side_effect = ["Bella458"]
         self.assertIn(GitApi567.getRepositories("SSW567"), "Bella458")
+
+    @mock.patch('GitApi567.getRepositories')   
+    def test_getRepositories2(self, mockedReq):
+        mockedReq.return_value = mock.Mock(["SSW567","HelloWorld","HK1","HK2","HK5"])
+        mockedReq.side_effect = ["Bella458"]
+        self.assertIn(GitApi567.getRepositories("HelloWorld"), "Bella458")
+
+
 
 if __name__ == '__main__':
     print('Running Unit Tests')
